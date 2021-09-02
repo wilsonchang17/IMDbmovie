5e95180bd317882b3544f96242579ff84b9d627c
Wilson <79236612+wilsonchang17@users.noreply.github.com>
2021-09-02 14:42:52 +0800

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-genre = ["Action","Adventure","Animation","Biography","Comedy","Crime","Drama","Family","Fantasy","Film-Noir","History","Horror","Music","Musical","Mystery","Romance","Sci-Fi","Sport","Thriller","War","Western"]
-genre2 = ["1:Action","2:Adventure","3:Animation","4:Biography","5:Comedy","6:Crime","7:Drama","8:Family","9:Fantasy","10:Film-Noir","11:History","12:Horror","13:Music","14:Musical","15:Mystery","16:Romance","17:Sci-Fi","18:Sport","19:Thriller","20:War","21:Western"]
+genre = ["Action","Adventure","Animation","Biography","Comedy","Crime","Drama","Family","Fantasy","History","Horror","Music","Musical","Mystery","Romance","Sci-Fi","Sport","Thriller","War","Western"]
+genre2 = ["1:Action","2:Adventure","3:Animation","4:Biography","5:Comedy","6:Crime","7:Drama","8:Family","9:Fantasy","10:History","11:Horror","12:Music","13:Musical","14:Mystery","15:Romance","16:Sci-Fi","17:Sport","18:Thriller","19:War","20:Western"]
 print("類型選擇:")
 for i in range(len(genre2)):
     print(genre2[i])
 print()
 name = input("請輸入查詢類型號碼: ")
-if(int(name)>21):
+if(int(name)>20):
     print("類型不符合")
 else:
     print()
@@ -111,4 +111,3 @@ else:
         print("大綱：",text[i])
         print("連結：",web[i])
         print()
-    
